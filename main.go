029618438782c9d48c3659b597e6c3fa4f1a26f3
ayumu83s <ayumu.itou.1125@gmail.com>
2018-08-26 17:52:17 +0900

    Add main

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ayumu83s/di_sample/controller"
+	"github.com/ayumu83s/di_sample/registory"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	repo := registory.NewRepository()
+	controller := controller.NewUser(repo)
+	controller.Get(1)
 }
